e96fddc2a898ac1134afc66297db44f34e744946
Lê Phương Khanh <khanhlp315@gmail.com>
2020-03-18 09:28:57 +0700

    init project

---
+++ Assets/TheHuntGame/Scripts/MainGame/MainGameLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TheHuntGame.EventSystem.Events;
+using UnityEngine;
+
+namespace TheHuntGame.MainGame
+{
+    public class MainGameLogic : MonoBehaviour
+    {
+        private void Awake()
+        {
+            EventSystem.EventSystem.Instance.Bind<CoinInsertEvent>(OnCoinInserted);
+        }
+
+        private void OnCoinInserted(CoinInsertEvent e)
+        {
+        }
+    }
+}
